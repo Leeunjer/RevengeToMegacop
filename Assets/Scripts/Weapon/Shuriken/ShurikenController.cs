c073598abd84731d41072248cb132fda38a9a843
ahn eun seong <tjddmsdks@gmail.com>
2026-02-18 00:36:02 +0900

    수리검 파괴 로직 추가

--- Assets/Scripts/Weapon/Shuriken/ShurikenController.cs
+++ Assets/Scripts/Weapon/Shuriken/ShurikenController.cs
@@ -4,6 +4,14 @@ public class ShurikenController : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
 
+    [SerializeField] private float lifeTime = 3f;
+
+
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+
     void Update()
     {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
