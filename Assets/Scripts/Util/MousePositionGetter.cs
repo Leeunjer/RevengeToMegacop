3a04cfcbe2535e56c5503f64037e9e83ecf32404
ahn eun seong <tjddmsdks@gmail.com>
2026-02-25 16:48:39 +0900

    MainCamera 캐싱 추가

--- Assets/Scripts/Util/MousePositionGetter.cs
+++ Assets/Scripts/Util/MousePositionGetter.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 
 public static class MousePositionGetter
 {
+    private static Camera mainCamera;
+
     public static Vector3 GetMousePositionInWorld(Vector3 target)
     {
+        if (mainCamera == null) mainCamera = Camera.main;
+
         Plane groundPlane = new Plane(Vector3.up, new Vector3(0, target.y, 0));
 
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
 
         if (groundPlane.Raycast(ray, out float enter))
         {
